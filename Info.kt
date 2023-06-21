0552f0bd68fe2d4e1b8f360fb28b211c02d72dcb
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2023-06-21 18:32:01 +0300

    fix Info

--- Info.kt
+++ Info.kt
@@ -1,13 +1,17 @@
 package calculator
 
 class Info {
-    private val mes = "The program calculates the sum and the difference of numbers"
-    private val mes2 = "If several identical statements are entered, following one another, the program still should work"
-    private val mes3 = "Two adjacent minus signs turn into a plus (-- -> +)"
-    private val mes4 = "Valid user input - only numbers, a plus sign, a minus sign, and two commands:"
-    private val mes5 = "\"/help\" - prints information about program"
-    private val mes6 = "\"/exit\" - the program stops"
+    private val mes = """
+        The calculator has operations: addition, subtraction, multiplication and division
+        A general expression can contain many parentheses and operations with different priorities
+        If several identical statements(+, -) are entered, following one another, the program still work
+        Two adjacent minus signs turn into a plus (-- -> +)
+        Valid user input - only numbers, a plus sign, a minus sign, and two commands:
+        "/help" - prints information about program
+        "/exit" - the program stops
+    """.trimIndent()
+
     internal fun printInfo() {
-        println("$mes\n$mes2\n$mes3\n$mes4\n$mes5\n$mes6")
+        println(mes)
     }
 }
